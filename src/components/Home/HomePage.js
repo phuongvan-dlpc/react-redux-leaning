179a0c8fcd9d955ab2592c22a34a1aea98b4bdd0
phuongvan-dlpc <phuongvd.dlpc@gmail.com>
2024-07-22 21:27:48 +0700

    ngay-22-7-2024-DesignHompage

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,7 +1,13 @@
+import videoHomepage from '../../assets/video-homepage.mp4';
 const HomePage = (props) => {
     return (
         <div className="homepage-container">
-            Homepage component
+            <video autoPlay muted loop>
+                <source
+                    src={videoHomepage}
+                    type="video/mp4"
+                />
+            </video>
         </div>
     )
 }
