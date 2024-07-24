49735a1ba3f50ffeb613d11d05ee38f40ca791ad
phuongvan-dlpc <phuongvd.dlpc@gmail.com>
2024-07-24 10:42:17 +0700

    ngay-24-7-2024-sidebarpro

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,7 +1,21 @@
+import SideBar from "./SideBar";
+import './Admin.scss';
+import { useState } from "react";
+import { FaBars } from "react-icons/fa";
+
 const Admin = (props) => {
+    const [collapsed, setCollapsed] = useState(false);
+    const [toggled, setToggle] = useState(false);
+
     return (
-        <div>
-            Admin component
+        <div className="admin-container">
+            <div className="admin-sidebar">
+                <SideBar collapsed={collapsed} />
+            </div>
+            <div className="admin-content">
+                <FaBars onClick={() => setCollapsed(!collapsed)} />
+                content here
+            </div>
         </div>
     )
 }
---
+++ src/components/Admin/SideBar.js
@@ -0,0 +1,82 @@
+import { Menu, MenuItem, ProSidebar, SidebarContent, SidebarFooter, SidebarHeader, SubMenu } from 'react-pro-sidebar';
+import 'react-pro-sidebar/dist/css/styles.css';
+import sidebarBg from '../../assets/sidebar-bg-img.jpg'
+import { FaTachometerAlt, FaGem, FaRegLaughWink, FaGithub } from "react-icons/fa";
+
+const SideBar = (props) => {
+    const { collapsed, toggled, handleToggleSidebar } = props;
+    return (
+        <>
+            <ProSidebar
+                image={sidebarBg}
+                collapsed={collapsed}
+                toggled={toggled}
+                breakPoint="md"
+                onToggle={handleToggleSidebar}
+            >
+                <SidebarHeader>
+                    <div
+                        style={{
+                            padding: '24px',
+                            textTransform: 'uppercase',
+                            fontWeight: 'bold',
+                            fontSize: 14,
+                            letterSpacing: '1px',
+                            overflow: 'hidden',
+                            textOverflow: 'ellipsis',
+                            whiteSpace: 'nowrap',
+                        }}
+                    >
+                        React sidebar
+                    </div>
+                </SidebarHeader>
+
+                <SidebarContent>
+                    <Menu iconShape="circle">
+                        <MenuItem
+                            icon={<FaTachometerAlt />}
+                            suffix={<span className="badge red">New</span>}
+                        >
+                            dashboard
+                        </MenuItem>
+                        <MenuItem icon={<FaGem />}> components </MenuItem>
+                    </Menu>
+                    <Menu iconShape="circle">
+                        <SubMenu
+                            suffix={<span className="badge yellow">3</span>}
+                            icon={<FaRegLaughWink />}
+                        >
+                            <MenuItem> 1</MenuItem>
+                            <MenuItem> 2</MenuItem>
+                            <MenuItem> 3</MenuItem>
+                        </SubMenu>
+
+                    </Menu>
+                </SidebarContent>
+
+                <SidebarFooter style={{ textAlign: 'center' }}>
+                    <div
+                        className="sidebar-btn-wrapper"
+                        style={{
+                            padding: '20px 24px',
+                        }}
+                    >
+                        <a
+                            href="https://github.com/azouaoui-med/react-pro-sidebar"
+                            target="_blank"
+                            className="sidebar-btn"
+                            rel="noopener noreferrer"
+                        >
+                            <FaGithub />
+                            <span style={{ whiteSpace: 'nowrap', textOverflow: 'ellipsis', overflow: 'hidden' }}>
+                                viewSource
+                            </span>
+                        </a>
+                    </div>
+                </SidebarFooter>
+            </ProSidebar>
+        </>
+    )
+}
+
+export default SideBar;
