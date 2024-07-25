fe2f266f7a852a45a4da30566c80dd3a7f35367a
phuongvan-dlpc <phuongvd.dlpc@gmail.com>
2024-07-25 15:02:59 +0700

    ngay-25-7-2024-state-modal-add-new

--- src/components/Admin/Content/ManageUser.js
+++ src/components/Admin/Content/ManageUser.js
@@ -1,17 +1,30 @@
 import ModalCreateUser from "./ModalCreateUser";
+import './ManageUser.scss';
+import { FcPlus } from "react-icons/fc";
+import { useState } from "react";
 
 const ManageUser = (props) => {
+    const [showModalCreateUser, setShowModalCreateUser] = useState(false);
     return (
         <div className="manage-user-container">
             <div className="title">
                 Manage User
             </div>
             <div className="user-content">
-                <button>Add new user</button>
-            </div>
-            <div>
-                Table users
-                <ModalCreateUser />
+                <div className="btn-add-new">
+                    <button className="btn btn-primary"
+                        onClick={() => setShowModalCreateUser(true)}
+                    >
+                        <FcPlus />Add new user
+                    </button>
+                </div>
+                <div className="table-users-container">
+                    Table users
+                </div>
+                <ModalCreateUser
+                    show={showModalCreateUser}
+                    setShow={setShowModalCreateUser}
+                />
             </div>
         </div>
     )
--- src/components/Admin/Content/ModalCreateUser.js
+++ src/components/Admin/Content/ModalCreateUser.js
@@ -1,24 +1,38 @@
 import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import { FcPlus } from "react-icons/fc";
 
-const ModalCreateUser = () => {
-    const [show, setShow] = useState(false);
 
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+const ModalCreateUser = (props) => {
+    const { show, setShow } = props;
+    const handleClose = () => setShow(false); //hàm setShow gọi từ parent
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [username, setUsername] = useState('');
+    const [role, setRole] = useState('USER');
+    const [image, setImage] = useState('');
+    const [previewImage, setPreviewImage] = useState('');
+
+    const handleUploadImage = (event) => {
+        if (event.target && event.target.files && event.target.files[0]) {
+            setImage(event.target.files[0]);
+            setPreviewImage(URL.createObjectURL(event.target.files[0]));
+        }
+
+        console.log("image", image);
+        console.log("image preview", previewImage);
+    }
 
     return (
         <>
-            <Button variant="primary" onClick={handleShow}>
-                Launch demo modal
-            </Button>
-
             <Modal
-                show={show}
+                show={show} //Gọi từ parent
                 onHide={handleClose}
                 size='xl'
                 backdrop="static"  //đặt thuộc tính này để click ngoài modal sẽ không bị đóng (Chỉ nhấn x, hoặc Close để đóng)
+                className='modal-add-user'
             >
                 <Modal.Header closeButton>
                     <Modal.Title>Add new user</Modal.Title>
@@ -27,34 +41,62 @@ const ModalCreateUser = () => {
                     <form className="row g-3">
                         <div className="col-md-6">
                             <label className="form-label">Email</label>
-                            <input type="email" className="form-control" />
+                            <input type="email" className="form-control"
+                                value={email}
+                                onChange={(event) => setEmail(event.target.value)}
+                            />
                         </div>
                         <div className="col-md-6">
                             <label className="form-label">Password</label>
-                            <input type="password" className="form-control" />
+                            <input type="password" className="form-control"
+                                value={password}
+                                onChange={(event) => setPassword(event.target.value)}
+                            />
                         </div>
                         <div className="col-md-6">
                             <label className="form-label">Username</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control"
+                                value={username}
+                                onChange={(event) => setUsername(event.target.value)}
+                            />
                         </div>
                         <div className="col-md-4">
                             <label className="form-label">Role</label>
-                            <select className="form-select">
+                            <select className="form-select"
+                                onChange={(event) => setRole(event.target.value)}
+                                value={role}
+                            >
                                 <option selected value="USER">USER</option>
                                 <option value="ADMIN">ADMIN</option>
                             </select>
                         </div>
                         <div className='col-md-12'>
-                            <label className="form-label"> Image </label>
-                            <input type='file' />
+                            <label className='form-label label-upload' htmlFor='labelUPload'><FcPlus />Upload file image</label>
+                            <input
+                                type='file'
+                                id='labelUPload'
+                                onChange={(event) => handleUploadImage(event)}
+                                hidden
+                            />
+                        </div>
+                        <div className='col-md-12 img-preview'>
+                            {previewImage ?
+                                <img src={previewImage} />
+                                :
+                                <span>Preview Image</span>
+                            }
                         </div>
                     </form>
                 </Modal.Body>
                 <Modal.Footer>
-                    <Button variant="secondary" onClick={handleClose}>
+                    <Button variant="secondary"
+                        onClick={handleClose}
+                    >
                         Close
                     </Button>
-                    <Button variant="primary" onClick={handleClose}>
+                    <Button variant="primary"
+                        onClick={handleClose}
+                    >
                         Save
                     </Button>
                 </Modal.Footer>
