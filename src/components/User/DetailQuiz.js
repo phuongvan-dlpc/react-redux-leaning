2b585041643b041f3ec2b462f1a088339da4aef7
phuongvan-dlpc <phuongvd.dlpc@gmail.com>
2024-08-04 21:20:19 +0700

    video-83

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { getDataQuiz } from "../../services/apiService";
+import _ from "lodash";
 
 const DetailQuiz = (props) => {
     const params = useParams();
@@ -12,7 +13,31 @@ const DetailQuiz = (props) => {
 
     const fetchQuestions = async () => {
         let res = await getDataQuiz(quizId);
-        console.log("check getDataQuiz: ", res);
+
+        if (res && res.EC === 0) {
+            let raw = res.DT;
+            let data = _.chain(raw)
+                // Group the elements of Array based on `color` property
+                .groupBy("id")
+                // `key` is group's name (color), `value` is the array of objects
+                .map((value, key) => {
+                    // console.log('value: ', value, '    key: ', key);
+                    let answers = [];
+                    let questionDescription, image = null;
+                    value.forEach((item, index) => {
+                        if (index === 0) {
+                            questionDescription = item.description;
+                            image = item.image;
+                        }
+                        answers.push(item.answers);
+                        // console.log('item answer: ', item.answers);
+                    })
+                    return { questionId: key, answers, questionDescription, image }
+                    //Viết đầy đủ return { questionId: key, answers: answers}
+                })
+                .value()
+            // console.log("check data lodash: ", data);
+        }
     }
 
     return (
